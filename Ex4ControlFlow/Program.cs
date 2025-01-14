9f85fcdd199f3207df0855ab5118471ba13f7112
Arif Iqbal <arif_iqbal@yahoo.com>
2025-01-14 10:24:23 -0600

    Modified the license suspension checker code

--- Ex4ControlFlow/Program.cs
+++ Ex4ControlFlow/Program.cs
@@ -12,52 +12,59 @@ namespace Ex4ControlFlow
             //Excercise - 4
             //Your job is to write a program for a speed camera.For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.Write a program that asks the user to enter the speed limit.Once set, the program asks for the speed of a car.If the user enters a value less than the speed limit, program should display Ok on the console.If the value is above the speed limit, the program should calculate the number of demerit points.For every 5km / hr above the speed limit, 1 demerit points should be incurred and displayed on the console.If the number of demerit points is above 12, the program should display License Suspended.
             
+            //  Incrementor: when car speed is 5 km/ hour or beyound
             int demeritDelta = 0;
-            bool demerit=true;
+
+            //  Programs Entry & Exit Bool
+            bool demerit = true;
+
+
             do
             {
-                
-            Console.WriteLine("Speed Limit Detactor!\n");
 
-            Console.WriteLine("Enter the Speed Limit");
+                Console.WriteLine("Speed Limit Detactor!\n");
 
-            int speedLimit = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Speed Limit");
+
+                int speedLimit = int.Parse(Console.ReadLine());
+
+                //  License Supension Checker and Exit when requirement are met
+                bool suspensionCheck = true;
 
                 do
                 {
-                Console.WriteLine("Enter your Car Speed");
+                    Console.WriteLine("Enter your Car Speed");
 
-                int carSpeedLimit = int.Parse(Console.ReadLine());
+                    int carSpeedLimit = int.Parse(Console.ReadLine());
 
-                int speedDifference;
-
-
-                Console.WriteLine($"\nDemerit Delta Top : {demeritDelta}");
+                    int speedDifference;
 
 
-                if (carSpeedLimit > speedLimit)
-                {
+                    Console.WriteLine($"\nDemerit Delta Top : {demeritDelta}");
+
+
+                    if (carSpeedLimit > speedLimit)
+                    {
                     speedDifference = carSpeedLimit - speedLimit;
                
-                if (speedDifference <= 12)
-                {
+                    if (speedDifference >= 5)
+                    {
 
                     demeritDelta ++;
 
-
                     Console.WriteLine($"\nDemerit Delta is :{demeritDelta}");
-                }
+                    }
 
-
-                if (demeritDelta >= 12)
-                {
+                    if (demeritDelta >= 12)
+                    {
                         Console.WriteLine("Your License has been Suspended");
+                        suspensionCheck = false;
                         demerit = false;
-                }
-                }
+                    }
+                    }
                     
                 }
-                while (   );
+                while (suspensionCheck);
 
 
 
