a25b2790421bd343ae3331e5825ff239bd9074db
Arif Iqbal <arif_iqbal@yahoo.com>
2025-01-15 17:08:13 -0600

    Post likeness and Max Number partial complete

---
+++ Ex1-Array_List/Program.cs
@@ -0,0 +1,57 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+
+namespace Ex1_Array_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fans Like Posting!");
+
+            //Exercise - 1:
+            //
+            //When you post a message on Facebook, depending on the number of people who like your post, Facebook displays different information.
+
+            //If no one likes your post, it doesn't display anything.
+            //If only one person likes your post, it displays: [Friend's Name] likes your post.
+            //If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
+            //If more than two people like your post, it displays: [Friend 1], [Friend 2] and[Number of Other People] others like your post.
+            //Write a program and continuously ask the user to enter different names, until the user presses Enter(without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
+
+            //  Storing UserName in the List
+            var userNameList = new List<string>();
+
+            //  Loop Exit condition;
+            bool userEntry = true;
+
+            do
+            {
+                Console.WriteLine("Enter User Name !!\n");
+
+                var userName = Console.ReadLine();
+
+                Console.WriteLine("Do you like my 'Facebook Post', Enter 'true' for Like 'false' for Dislike\n");
+
+                string userLikeness = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(userName) || (string.IsNullOrEmpty(userLikeness)) && (userLikeness == "0"))
+                {
+                    userEntry = false;
+                }
+                else if(!string.IsNullOrEmpty(userName) && (userLikeness == "1"))
+                {
+                        userNameList.Add(userName);
+                }
+            }
+            while (userEntry);
+
+            //Displaying the Username that likes the POST.
+            foreach (var userName in userNameList) 
+            { 
+                Console.WriteLine(userName);
+            }
+            //This code can be refactored and post likness can be prompted after adding the username in the list
+        }
+    }
+}
---
+++ Ex9-ControlFlow/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+
+namespace Ex9_ControlFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Display the Maximum Number!");
+
+            //Excercise - 10:
+            //Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+
+            Console.WriteLine("Enter serires of numbers separated by comma");
+
+           
+            // Taking the userInput
+
+            string userInput = Console.ReadLine();
+
+            var userStrings = userInput.Split(',');
+
+            var numberList = new List<int>();
+
+            foreach (var userString in userStrings)
+            {
+                Console.WriteLine(userString);
+
+                var userNumber = Int32.Parse(userString);
+
+                for (int i = 0; i < numberList.Count; i++)
+                {
+
+                    if (userNumber >= 0)
+                    {
+                        numberList.Add(userNumber);
+                        continue;
+                    }
+
+                
+                }
+
+
+                
+
+            }
+
+            
+        }
+    }
+}
