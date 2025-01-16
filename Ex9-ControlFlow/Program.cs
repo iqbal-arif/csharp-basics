33c91e0f7a589a0b8ba1f8a7c3011e27fa02b34d
Arif Iqbal <arif_iqbal@yahoo.com>
2025-01-15 18:08:42 -0600

    Max Number completed

--- Ex9-ControlFlow/Program.cs
+++ Ex9-ControlFlow/Program.cs
@@ -8,46 +8,35 @@ namespace Ex9_ControlFlow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Display the Maximum Number!");
+            Console.WriteLine("Display the Maximum Number!\n");
 
             //Excercise - 10:
             //Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
 
-            Console.WriteLine("Enter serires of numbers separated by comma");
+            Console.WriteLine("Enter serires of numbers separated by comma\n");
 
-           
             // Taking the userInput
 
             string userInput = Console.ReadLine();
 
             var userStrings = userInput.Split(',');
 
-            var numberList = new List<int>();
+            int maxNumber = 0;
+
+            //var numberList = new List<int>();
 
             foreach (var userString in userStrings)
             {
-                Console.WriteLine(userString);
 
-                var userNumber = Int32.Parse(userString);
+                var userNumber = int.Parse(userString);
 
-                for (int i = 0; i < numberList.Count; i++)
+                if(userNumber > maxNumber)
                 {
-
-                    if (userNumber >= 0)
-                    {
-                        numberList.Add(userNumber);
-                        continue;
-                    }
-
-                
+                    maxNumber = userNumber;
                 }
-
-
-                
-
             }
 
-            
+            Console.WriteLine($"\nThe Maximum Number is : {maxNumber}\n");
         }
     }
 }
