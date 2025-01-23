e9f391886e635249b0b12099bff98fcce293aabf
Arif Iqbal <arif_iqbal@yahoo.com>
2025-01-23 11:19:14 -0600

    Display total number of words in the file

---
+++ Ex1-WorkingwithFiles/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+
+namespace Ex1_WorkingwithFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read File & Display Number of Words ! \n");
+
+            //1 - Write a program that reads a text file and displays the number of words.
+
+            var path = @"C:\Source\csharp-basics\Ex1-WorkingwithFiles\ex1.readword.txt";
+            var content = File.ReadAllText(path) ;
+            //Console.WriteLine(content) ;
+
+            string[] fileContent = content.Split(" ");
+
+            foreach (string file in fileContent)
+            {
+                Console.WriteLine(file);
+            }
+
+
+            int count = 0;
+
+            foreach (string word in fileContent)
+            {
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"\nThe total number of words in this file is : {count} !!");
+        }
+    }
+}
