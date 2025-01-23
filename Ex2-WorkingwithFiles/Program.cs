e3866c4c6a5f2f8c06e8c556b967179be8a67177
Arif Iqbal <arif_iqbal@yahoo.com>
2025-01-23 14:55:50 -0600

    Longest word in the wordList

---
+++ Ex2-WorkingwithFiles/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace Ex2_WorkingwithFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Displays Longest Word in file!\n");
+
+            //2 - Write a program that reads a text file and displays the longest word in the file.
+
+            var path = @"C:\Source\csharp-basics\Ex1-WorkingwithFiles\ex1.readword.txt";
+
+            var fileContent = File.ReadAllText(path);
+
+            //Console.WriteLine(fileContent);
+
+            string[] wordList = fileContent.Replace("\r", "").Replace("\n", " ").Split(" ");
+
+            // Setting the length of the Longest WpORD LENGHT
+            int longestWordLength = 0;
+
+            // Variable to set Long Word
+            string longWord = " ";
+
+            foreach (string word in wordList)
+            {
+                // Setting the Word length after looping words in the wordList
+                int wordLength = word.Length;
+
+                if (wordLength > longestWordLength)
+                {
+                    longestWordLength = word.Length;
+                    longWord = word;
+                }
+
+            }
+
+
+            Console.WriteLine($"\nThe longest word is : {longWord}");
+        }
+    }
+}
